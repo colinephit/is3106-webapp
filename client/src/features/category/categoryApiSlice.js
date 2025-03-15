48db0f100136713f20b3a1a3384b3f0fe4dea878
colinephit <colinephitoyo@gmail.com>
2025-03-15 23:41:27 +0800

    added missing files

---
+++ client/src/features/category/categoryApiSlice.js
@@ -0,0 +1,31 @@
+import { apiSlice } from "../../redux/apiSlice";
+
+export const categoryApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getCategories: builder.query({
+      query: (search) => `/category/search?search=${search}`, // Adjust endpoint accordingly
+      providesTags: ["category"],
+    }),
+    addCategory: builder.mutation({
+      query: (newCategory) => ({
+        url: "/category/create",
+        method: "POST",
+        body: newCategory,
+      }),
+      invalidatesTags: ["category"],
+    }),
+    getAllCategories: builder.query({
+      query: () => ({
+        url: "/category/list",
+        method: "GET",
+      }),
+      providesTags: ["category"],
+    }),
+  }),
+});
+
+export const {
+  useGetCategoriesQuery,
+  useAddCategoryMutation,
+  useGetAllCategoriesQuery,
+} = categoryApiSlice;
